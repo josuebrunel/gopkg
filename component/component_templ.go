8a82111c418b4a60bef28300dce195a766bc7dd5
Josue Kouka <josuebrunel@gmail.com>
2024-07-14 20:51:57 -0700

    component: fix if component

--- component/component_templ.go
+++ component/component_templ.go
@@ -26,7 +26,7 @@ func If(cond bool, comp templ.Component) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		if bool {
+		if cond {
 			templ_7745c5c3_Err = comp.Render(ctx, templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
